01bd552f5629cae6db866a34027d1a8319ff43d9
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-02-24 08:36:24 +0600

    Add files via upload

---
+++ class7.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './viewsClass7'); // Corrected path to the views directory
+
+app.get('/home', function(req, res) {
+    res.render('home',{
+        title:"Home Page",
+   users:{name:"Khalid",age:"21"}
+    });
+});
+
+app.listen(3000);
